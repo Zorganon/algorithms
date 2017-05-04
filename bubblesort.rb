cc99262cdc78c854bf1691d8efb0fe8dedaa0bb4
Zorganon <dan.the.hudson@gmail.com>
2017-05-04 09:23:54 -0500

    bubble sort algorithm successful

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,22 +1,24 @@
-def bubbleSort(arry)
-	finished_yet = false
-	unless finished_yet = true
-		spot1=-1
-		spot2=0
-		finished_yet = true
-		while spot2<arry.length
-			spot1 +=1
-			spot2 +=1
-			if arry[spot1]>arry[spot2]
-				arry.delete_at(spot2).insert!(spot1)
-				finished_yet = false
+def bubbleSort(barry)
+	l = barry.length * 2
+	sortedArry = barry
+	while l >= 0
+		l -= 1
+		sortedArry.each.with_index do |x, i| 
+			if i == sortedArry.length - 1
+				next
+			end
+			y = sortedArry[i+1]
+			if x > y
+				sortedArry[i] = y
+				sortedArry[i+1] = x
 			end
 		end
 	end
-	return arry
+	print " #{sortedArry} \n "
+
 end
 
-test_arry = [8,4,2,6,1,3]
+test_arry = [8,4,2,0,9,5,0,1,10,9,6,1,1,1,1,6,1,3]
 test_arry2 = [5,4,6,7,3,8,2,5,1,4,7,4,8,9,5,7,3,8,5,0,1,5,3,7,4]
 
 bubbleSort(test_arry)
